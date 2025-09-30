f3dd4b85674213df82dbafe7a3d86789cc1cd58c
Paulina Herrera <al2182000702@azc.uam.mx>
2025-09-30 13:44:09 -0600

    Desarrollo de package customer

---
+++ proyecto_final/banco/src/main/java/com/proyecto/banco/customer/dto/cutomerResponse.java
@@ -0,0 +1,4 @@
+package com.proyecto.banco.customer.dto;
+
+public class cutomerResponse {
+}
--- proyecto_final/banco/src/main/java/com/proyecto/banco/customer/repository/customerRepo.java
+++ proyecto_final/banco/src/main/java/com/proyecto/banco/customer/repository/customerRepo.java
@@ -1,4 +1,25 @@
 package com.proyecto.banco.customer.repository;
 
-public interface customerRepo {
+import com.proyecto.banco.customer.model.customerModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface customerRepo extends JpaRepository<customerModel, Long>  {
+
+    // Buscar cliente por email
+    Optional<customerModel> findByEmail(String email);
+
+    // Verificar si existe un email
+    boolean existsByEmail(String email);
+
+    // Buscar clientes por estado
+    List<customerModel> findByStatus(customerModel.CustomerStatus status);
+
+    // Buscar clientes por nombre (búsqueda parcial, case-insensitive)
+    List<customerModel> findByNameContainingIgnoreCase(String name);
+
 }
